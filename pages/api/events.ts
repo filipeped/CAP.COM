558d6e0c731f4f5d5dfe1f0a73c20d14039c9e12
filipeped <filipepedoo@gmail.com>
2025-08-30 02:15:28 -0300

    Update events.ts

--- pages/api/events.ts
+++ pages/api/events.ts
@@ -12,7 +12,7 @@ import crypto from "crypto";
 import zlib from "zlib";
 
 const PIXEL_ID = "765087775987515";
-const ACCESS_TOKEN = "EAAQfmxkTTZCcBPHGbA2ojC29bVbNPa6GM3nxMxsZC29ijBmuyexVifaGnrjFZBZBS6LEkaR29X3tc5TWn4SHHffeXiPvexZAYKP5mTMoYGx5AoVYaluaqBTtiKIjWALxuMZAPVcBk1PuYCb0nJfhpzAezh018LU3cT45vuEflMicoQEHHk3H5YKNVAPaUZC6yzhcQZDZD";
+const ACCESS_TOKEN = "EAAQfmxkTTZCcBPXwanbgun3ZA4j5ZBkZCoZAIsZBb3klnfSc5fEp67hpwR2J6RmXLZBuAuQxPA3F3PJPiqhHdi3sZA9FQ8yA5tHxiGyXQpXvtAeV4W06bVzZAoSiUkAX97m0qZCMSZCMunZAQRehjXTccYNcWNeLcKpUDvy9ZCDOnd3tLiH5MLp0EyGAiwK3avTDsnAZDZD";
 const META_URL = `https://graph.facebook.com/v19.0/${PIXEL_ID}/events`;
 
 // ✅ SISTEMA DE DEDUPLICAÇÃO
